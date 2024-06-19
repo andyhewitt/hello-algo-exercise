26e8a43464552dead06a36d96c6ef4ccce39fd34
andyhewitt <andyhewitt1987@gmail.com>
2024-06-19 21:57:09 +0900

    New round of job hunting starts.

---
+++ BFS/bfs.py
@@ -0,0 +1,53 @@
+"""BFS implementation"""
+from collections import deque
+
+
+class TreeNode:
+    """二叉树节点类"""
+
+    def __init__(self, val: int):
+        self.val: int = val                # 节点值
+        self.left: TreeNode | None = None  # 左子节点引用
+        self.right: TreeNode | None = None  # 右子节点引用
+
+
+def level_order(root: TreeNode | None) -> list[int]:
+    """层序遍历"""
+    # 初始化队列，加入根节点
+    queue: deque[TreeNode] = deque()
+    queue.append(root)
+    # 初始化一个列表，用于保存遍历序列
+    res = []
+
+    # while the list is not empty, if there is a child node on the left, put it into the list
+    while len(queue) != 0:
+        node = queue.popleft()
+        res.append(node.val)
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
+
+    return res
+
+
+# 初始化二叉树
+# 初始化节点
+n1 = TreeNode(val=1)
+n2 = TreeNode(val=2)
+n3 = TreeNode(val=3)
+n4 = TreeNode(val=4)
+n5 = TreeNode(val=5)
+n6 = TreeNode(val=6)
+n7 = TreeNode(val=7)
+# 构建节点之间的引用（指针）
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.left = n6
+n3.right = n7
+
+
+resList = level_order(n1)
+print(resList)
